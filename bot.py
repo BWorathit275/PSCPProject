69dd1e8f26444caaf25a8547fb3a12beec2bd48c
Bwora <67070275@kmitl.ac.th>
2024-10-06 18:19:52 +0700

    added secure token code location

--- bot.py
+++ bot.py
@@ -1,15 +1,24 @@
+import os
 import discord
 from discord.ext import commands
+from dotenv import load_dotenv
 
-client = commands.Bot(commands_prefix = '!')
+# Load environment variables from a .env file
+load_dotenv()
+tokencode = os.getenv('DISCORD_TOKEN')
+
+client = commands.Bot(command_prefix='!')
 
 @client.event
 async def on_ready():
+    """status"""
     print("Bot is Ready")
     print(":)")
 
 @client.command()
-async def testcommand(ctx):
-    await ctx.send("The One Piece,  Is Real")
-    
-@client.run('tokencode')
+async def hi(ctx):
+    """testcommand"""
+    await ctx.send("The One Piece, Is Real")
+
+# Run the bot with the token from the environment variable
+client.run(tokencode)
