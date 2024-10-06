dce18e3c4763fb00c8388a7a4d1e4411f91d7f7b
Bwora <67070275@kmitl.ac.th>
2024-10-06 17:59:38 +0700

    added --@client.event

--- bot.py
+++ bot.py
@@ -1,8 +1,10 @@
 import discord
 from discord.ext import commands
 
-cilent = commands.Bot(commands_prefix = '!')
+client = commands.Bot(commands_prefix = '!')
 
-
-
+@client.event
+async def on_ready():
+    print("Bot is Ready")
+    print(":)")
 
